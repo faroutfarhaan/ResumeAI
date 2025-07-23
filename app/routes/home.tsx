453891d48d79e10464a9bd4da1c1f4b1c864d267
faroutfarhaan <133531252+faroutfarhaan@users.noreply.github.com>
2025-07-23 23:13:53 +0530

    added navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeAi" },
+    { name: "description", content: "Your personal AI for resume polishing." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url(/images/bg-main.svg)] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review you submissions and check AI-powered feedback</h2>
+      </div>
+    </section>
+  </main>;
 }
